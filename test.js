2afcd2280ce6917d9530b9c3bde80e6110091e20
KunigiMohan <kmohanr5@gmail.com>
2021-12-10 12:27:14 +0530

    Added : Using promise call to handle multiple call backs

--- test.js
+++ test.js
@@ -1,46 +1,57 @@
+function showTime() {
+    const date = new Date();
+    return date.getHours() + "Hrs: "+ d
+}
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-function makeAJAXCall(methodType, url, callback, async = true, data = null){
-    let xhr =new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
+function makePromiseCall(methodType, url, async = true, data = null){
+    return new Promise(function(resolve,reject){
+        let xhr =new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
         console.log("State Changed Called. Ready State: "+ xhr.readyState +"status: "+xhr.status);
-        if (xhr.readyState === 4 ) {
-            if (xhr.status === 200 || xhr.status ===201){
-                callback(xhr.responseText);
+            if (xhr.status.toString().match('^[2][0-9]{2}$')){
+                resolve(xhr.responseText);
             }
-            else if (xhr.status >= 400){
-                console.log("Handle 400 Client Error or 500 Server Error");
+            else if (xhr.status.toString().match('^[4,5][0-9]{2}$')){
+                reject ({
+                    status:xhr.status,
+                    statusText:xhr.statusText
+                });
+                console.log("XHR Failed");
             }
         }
-    }
-    xhr.open(methodType, url, async);
-    if(data){
-        console.log(JSON.stringify(data));
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify(data));
-    }
-    else xhr.send();
-    console.log(methodType+" request sent to the server");
+        xhr.open(methodType, url, async);
+        if(data){
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        }
+        else xhr.send();
+        console.log(methodType+" request sent to the server");
+    });
 }
-/**Declaring local host and function to get details of the file */
-const getURL = "http://localhost:3000/employees/";
-function getUserDetails(data){
-    console.log("Get User Data: "+data);
-}
-/**AJAX call for getting details */
-makeAJAXCall("GET", getURL, getUserDetails,true);
 
-/**Declaring local host for deleting data */
-const deleteURL = "http://localhost:3000/employees/2";
-function userDeleted(data){
-    console.log("User Deleted "+data);
-}
-/**Ajax Call for deleting specific data */
-makeAJAXCall("DELETE",deleteURL,userDeleted,false);
-/**Declaring local host for entering employee details */
+/**Declaring local host and function to get details of the file */
+const getURL = "http://localhost:3000/employees/4";
+/**making promise call to get details */
+makePromiseCall("GET",getURL,true)
+    .then(responseText => {
+        console.log("Get User Data at : "+ showTime()+" data "+ responseText);
+    })
+    .catch(error => console.log("GET Error Status : "+JSON.stringify(error)));
+
+/*calling makePromise call to delete elemnt */
+const deleteURL = "http://localhost:3000/employees/4";
+makePromiseCall("DELETE",deleteURL,true)
+    .then(responseText => {
+        console.log("Delete User Data at : "+ showTime()+" data "+ responseText);
+    })
+    .catch(error => console.log("GET Error Status : "+JSON.stringify(error)));
+
+/*sending POST promise call */
 const postURL = "http://localhost:3000/employees";
-const empData = {"name ":"Harry","salary":"6000"};
-function userAdded(data){
-    console.log("User Added : " +data);
-}
-/**Ajax call for entering data to JSON file */
-makeAJAXCall("POST",postURL,userAdded,true,empData);
+const empData = {"name" : "Rohi" ,"salary":"6000"};
+makePromiseCall("POST",postURL,true,empData)
+    .then(responseText => {
+        console.log("Post User Data at : "+ showTime()+" data "+ responseText);
+    })
+    .catch(error => console.log("GET Error Status : "+JSON.stringify(error)));
